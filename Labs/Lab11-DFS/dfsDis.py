c59cfca2452da3103b739ca65fe8457bbf533b1b
Abenezer <abnzr.dev@gmail.com>
2025-11-26 21:00:35 +0500

    add depth-first search implementation for disconnected graphs

---
+++ Labs/Lab11-DFS/dfsDis.py
@@ -0,0 +1,32 @@
+# DFS for disconnected graph
+# for this one we are doing for disconnected graphs
+
+def addedge(adj, u, v):
+    adj[u].append(v)
+    adj[v].append(u)
+
+def dfsrec(adj, source, visited):
+    visited[source] = True
+    print(source, end=" ")
+
+    for neigh in adj[source]:
+        if not visited[neigh]:
+            dfsrec(adj, neigh, visited)
+
+def dfs(adj):
+    visited = [False] * len(adj)
+    for u in range(len(adj)):
+        if not visited[u]:
+            dfsrec(adj, u, visited)
+
+if __name__ == "__main__":
+
+    edge = int(input())
+    vertex = int(input())
+    adj = [[] for i in range(vertex)]
+
+    for i in range(edge):
+        u, v = map(int, input().split())
+        addedge(adj, u, v)
+
+    dfs(adj)
