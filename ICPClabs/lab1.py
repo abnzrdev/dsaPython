75e1a8cf39507c2a309d142213866745f885e280
abnzr <abnzr.dev@gmail.com>
2026-01-13 10:22:42 +0500

    add type hints to Math class methods and implement totient calculator

--- ICPClabs/lab1.py
+++ ICPClabs/lab1.py
@@ -1,6 +1,6 @@
 class Math:
     # Problem A: Naive solution
-    def divisors(self, num):
+    def divisors(self, num: int) -> int:
         count = 0
         for i in range(1, num + 1):
             if num % i == 0:
@@ -8,7 +8,7 @@ class Math:
         return count - 2  # exclude 1 and num itself
 
     # Problem A: Efficient solution
-    def divisors_efficient(self, num):
+    def divisors_efficient(self, num: int) -> int:
         count = 0
         i = 1
         while i * i <= num:
@@ -21,7 +21,7 @@ class Math:
         return count - 2  # exclude 1 and num itself
 
     # Problem B: Prime factorization
-    def primefactors(self, num):
+    def primefactors(self, num: int) -> list[tuple[int, int]]:
         factors = []
 
         # Step 1: factor out 2
@@ -50,7 +50,7 @@ class Math:
         return factors
 
     # Problem C : Sums
-    def sum(self, num):
+    def sum(self, num: int) -> list[int] | None:
         k = 2
         result = []
         while k < num:
@@ -64,7 +64,7 @@ class Math:
         return None
 
     # Problem D : Stepan and pairs
-    def pairs(self, num):
+    def pairs(self, num: int) -> int:
         result = 0
         for i in range(1, num + 1):
             result += num // i
@@ -72,7 +72,7 @@ class Math:
         return result
 
     # Problem E : Sieve of Eratosthenes
-    def sieve(self, num):
+    def sieve(self, num: int) -> list[int]:
         is_prime = [True for i in range(num + 1)]
         is_prime[0] = is_prime[1] = False
 
@@ -84,7 +84,7 @@ class Math:
         return  [k for k in range(num + 1) if is_prime[k]]
 
     # Problem G : Almost Prime
-    def is_prime(self, num):
+    def is_prime(self, num: int) -> bool:
         if num < 2:
             return False
         for i in range(2, int(num ** 0.5) + 1):
@@ -92,7 +92,7 @@ class Math:
                 return False
         return True
 
-    def almost_prime(self, low, high):
+    def almost_prime(self, low: int, high: int) -> int:
         seen = set()
         for p in range(2, int(high ** 0.5) + 1):
             if self.is_prime(p):
@@ -104,7 +104,7 @@ class Math:
         return len(seen)
 
    # Problem H : Fast Exponentiation
-    def fst_expo(self, num):
+    def fst_expo(self, num: int) -> str:
         binary = str(bin(num))
         result = ""
         for i in range(2, len(binary)):
@@ -113,22 +113,37 @@ class Math:
             else:
                 result += "S"
         return result
-    def gcd(self, num1, num2):
+    def gcd(self, num1: int, num2: int) -> int:
         num1, num2 = abs(num1), abs(num2)
         while num2:
             num1, num2 = num2, num1 % num2
         return num1
 
-    def relative_prime(self, num):
+    def relative_prime(self, num: int) -> int:
         count = 0
         for i in range(1, num + 1):
             if self.gcd(num, i) == 1:
                 count += 1
         return count
 
+    # Problem J : Totient Calculator
+    def totient_calc(self, num: int) -> float:
+        prime_facts = self.primefactors(num)
+        factors = []
+        indx = 0
+        for tup in prime_facts:
+            factors.append(tup[indx])
+
+        totient = num
+        for i in factors:
+            totient *= (1 - (1 / i))
+
+        return totient
+
+
 if __name__ == "__main__":
     math = Math()
-    num1 = int(input(""))
-    answer = math.relative_prime(num1)
-    print(answer)
+    num1 = int(input("Enter a number : "))
+    totient = int(math.totient_calc(num1))
+    print(totient)
         
